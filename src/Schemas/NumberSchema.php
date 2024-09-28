c3a6c4d7e9a62320f0c382b4e0c736f0f770b6ad
Mekhdi <mehdiarsahanov@gmail.com>
2024-09-28 21:09:08 +0400

    divison of responsibility

--- src/Schemas/NumberSchema.php
+++ src/Schemas/NumberSchema.php
@@ -4,73 +4,21 @@ namespace Hexlet\Validator\Schemas;
 
 class NumberSchema extends ParentSchema
 {
-    private bool $shouldBePositive = false;
-    private float|int $min = -INF;
-    private float|int $max = INF;
-    private static array $rules = [];
-    private string $nameOfActiveRule = '';
-    private int $num;
-
-    public function isValid(?int $num): bool
+    public function required(): self
     {
-        return $this->isValueValid($num)
-            && $this->isPositive($num)
-            && $this->isAccordingRule($num)
-            && (int) $num >= $this->min
-            && (int) $num <= $this->max;
+        $this->rules['required'] = fn($num) => is_integer($num);
+        return $this;
     }
 
     public function positive(): self
     {
-        $this->shouldBePositive = true;
+        $this->rules['positive'] = fn($num) => $num > 0 || is_null($num);
         return $this;
     }
 
     public function range(int $min, int $max): self
     {
-        $this->min = $min;
-        $this->max = $max;
+        $this->rules['range'] = fn($num) => $num >= $min && $num <= $max;
         return $this;
-    }
-
-    public static function addRule(string $name, callable $fn): void
-    {
-        self::$rules[$name] = $fn;
-    }
-
-    public function test(string $functionName, int $num): self
-    {
-        $this->nameOfActiveRule = $functionName;
-        $this->num = $num;
-        return $this;
-    }
-
-    public function isValueValid(?int $num): bool
-    {
-        if (!$this->requirement) {
-            return true;
-        }
-
-        return is_integer($num);
-    }
-
-    public function isPositive(?int $num): bool
-    {
-        if (!$this->shouldBePositive) {
-            return true;
-        }
-
-        return $num > 0 || is_null($num);
-    }
-
-    public function isAccordingRule(?int $num): bool
-    {
-        $fn = self::$rules[$this->nameOfActiveRule] ?? null;
-
-        if (is_null($fn)) {
-            return true;
-        }
-
-        return $fn($num, $this->num);
     }
 }
